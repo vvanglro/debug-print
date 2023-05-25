f9353b611e48bfc431281862ee4b6b604ac6fb69
vvanglro <947001731@qq.com>
2023-05-25 10:42:08 +0800

    feat: add color

--- debug_print/__init__.py
+++ debug_print/__init__.py
@@ -1,17 +1,19 @@
 import inspect
 import builtins
 
-__version__ = "0.1.1"
-
-original_print = builtins.print
+__version__ = "0.1.2"
 
 
-def print_hook(*args, **kwargs):
-    frame = inspect.currentframe().f_back
-    file_name = frame.f_code.co_filename
-    line_num = frame.f_lineno
-    new_args = ("[{}:{}]".format(file_name, line_num),) + args
-    original_print(*new_args, **kwargs)
+def patch():
+    original_print = builtins.print
+    green = "\033[1;32m"
+    reset = "\033[0m"
 
+    def print_hook(*args, **kwargs):
+        frame = inspect.currentframe().f_back
+        file_name = frame.f_code.co_filename
+        line_num = frame.f_lineno
+        new_args = (green + "[{}:{}]".format(file_name, line_num) + reset,) + args
+        original_print(*new_args, **kwargs)
 
-builtins.print = print_hook
+    builtins.print = print_hook
